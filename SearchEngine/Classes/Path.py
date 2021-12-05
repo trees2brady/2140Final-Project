6cef6be53c1e317561dfdbf803408eb6b1e4a825
Xiaoye Mo <melico.moxy@gmail.com>
2021-12-05 14:38:38 -0500

    Update path

--- SearchEngine/Classes/Path.py
+++ SearchEngine/Classes/Path.py
@@ -1,11 +1,17 @@
-from final_project.settings import BASE_DIR
+import sys
 import os
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
+from final_project.settings import BASE_DIR
 
 # address of generated Text index file.
-IndexTextDir = os.path.join(BASE_DIR, "SearchEngine\\IndexConstruction\\data\\index\\")
+IndexTextDir = os.path.join(BASE_DIR, "SearchEngine", "IndexConstruction", "data", "index")
 
 # address of stopwords
-StopwordDir = "data//stopword.txt"
+StopwordDir = os.path.join(BASE_DIR, "SearchEngine", "IndexConstruction", "data", "stopword.txt")
 
-XMLPath = "data//xml_path.txt"
+XMLPath = os.path.join(BASE_DIR, "SearchEngine", "IndexConstruction", "data", "xml_path.txt")
+
 print(IndexTextDir)
+print(StopwordDir)
+print(XMLPath)
