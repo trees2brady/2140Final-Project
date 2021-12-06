50bdcf4ddce7796b223e33e748dbfbee7969983e
Xiaoye Mo <melico.moxy@gmail.com>
2021-12-06 01:55:02 -0500

    Update highlighting and preprocessing for input query

--- SearchEngine/Searching/QueryRetreival.py
+++ SearchEngine/Searching/QueryRetreival.py
@@ -5,13 +5,17 @@ from Classes import Path
 import whoosh.index as index
 from whoosh.qparser import QueryParser
 from whoosh import scoring
-
+from IndexConstruction.Indexing.StopWordRemover import StopWordRemover
+from IndexConstruction.Indexing.WordTokenizer import WordTokenizer
+from IndexConstruction.Indexing.WordNormalizer import WordNormalizer
 
 class QueryRetrieval:
 
     def __init__(self):
         self.path_dir = Path.IndexTextDir
         self.original_dir = Path.OriginalFilePath
+        self.stop_word_remover = StopWordRemover()
+        self.word_normalizer = WordNormalizer()
 
     def adv_query(self, query, topN):  # 这里写根据不同字段查询的逻辑
         try:
@@ -52,7 +56,7 @@ class QueryRetrieval:
             if "criteia" not in res:
                 res["criteia"] = ""
             print(description)
-            # set highlight keywords
+            # set description
             description += " ..."
 
             if summary:
@@ -62,14 +66,28 @@ class QueryRetrieval:
         except Exception as e:
             print(e)
             if summary:
-                res = {"title": "", "docId": "", "highlight": "", "description": ""}
+                res = {"title": "", "docId": "", "description": ""}
             else:
                 res = {"title": "", "contents": "No such file."}
 
         return res
     
-    def search(self,query, topN):
+    def search(self, original_query, topN):
         return_res = []
+
+        # process input query
+        word_tokenizer = WordTokenizer(original_query)
+        tokenized_word = word_tokenizer.nextWord()
+        words_list = []
+        while tokenized_word is not None and len(tokenized_word) != 0:
+            if not self.stop_word_remover.isStopword(tokenized_word):
+                normalized_word_lowercase = self.word_normalizer.lowercase(tokenized_word)
+                normalized_word_lowercase_stem = self.word_normalizer.stem(normalized_word_lowercase)
+                words_list.append(normalized_word_lowercase_stem)
+            tokenized_word = word_tokenizer.nextWord()
+        query = " ".join(words_list)
+        print(f"Processing Query: {query}")
+
         myindex = index.open_dir(self.path_dir)
         with myindex.searcher(weighting=scoring.BM25F(B=0.75, content_B=1.0, K1=1.5)) as self_seacher:
             query_parser = QueryParser("breif_summary", myindex.schema)
@@ -84,8 +102,15 @@ class QueryRetrieval:
                 res_dict = {}
                 res_dict["title"] = res["official_title"]
                 res_dict["docID"] = res["nct_id"]
-                res_dict["highlight"] = res["description"].split(" ")[:2]
                 res_dict["description"] = res["description"].replace("\n", " ")
+
+                # manage highlight keywords
+                highlight = []
+                for word in original_query.split(" "):
+                    if word in res["description"]:
+                        highlight.append(word)
+                res_dict["highlight"] = highlight
+                
                 return_res.append(res_dict)
         return return_res
 
